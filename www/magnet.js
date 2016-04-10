0df9ac7005a82d8698e3a10138c4b71482ba3474
unknown <Hesami@HHBMAG-HESAMI.hasingroup.com>
2016-04-10 16:10:17 +0430

    First release(beta)

--- www/magnet.js
+++ www/magnet.js
@@ -1,55 +1,61 @@
 var magnet = {
     Position: {
-        TOP_LEFT: 1,
-        TOP_CENTER: 2,
-        TOP_RIGHT: 3,
-        MIDDLE_LEFT: 4,
-        MIDDLE_CENTER: 5,
-        MIDDLE_RIGHT: 6,
-        BOTTOM_LEFT: 7,
-        BOTTOM_CENTER: 8,
-        BOTTOM_RIGHT: 9,
-        TOP_APP:10,
-        BOTTOM_APP:11
-    },
-    BannerSize: {
-        BANNER: {width:320,height:50},
-        IAB_MRECT:{width:300,height:250},
-        IAB_BANNER: {width:468,height:60},
-        IAB_LEADERBOARD: {width:728,height:90},
-        SMART_BANNER: {width:-1,height:-2},
-        IAB_WIDE_SKYSCRAPER: {width:160,height:600},
-        IPHONE_LANDSCAPE: {width:480,height:32},
-        IPAD_PORTRAIT: {width:769,height:90},
-        IPAD_LANDSCAPE: {width:1024,height:90}
+        TOP: 100,
+        BOTTOM: 101
     },
     Event: {
-        onBannerDismiss: "onAdmobBannerDismiss",
-        onBannerFailedReceive: "onAdmobBannerFailedReceive",
-        onBannerLeaveApplication: "onAdmobBannerLeaveApplication",
-        onBannerPresent: "onAdmobBannerPresent",
-        onBannerReceive: "onAdmobBannerReceive",
-        onInterstitialDismiss: "onAdmobInterstitialDismiss",
-        onInterstitialFailedReceive: "onAdmobInterstitialFailedReceive",
-        onInterstitialLeaveApplication: "onAdmobInterstitialLeaveApplication",
-        onInterstitialPresent: "onAdmobInterstitialPresent",
-        onInterstitialReceive: "onAdmobInterstitialReceive"
+        onInterstitialAdReceived :"onMagnetInterstitialAdReceived",
+        onInterstitialAdFailed :"onMagnetInterstitialAdFailed",
     },
-    Error:["ERROR_CODE_ERNAL_ERROR","ERROR_CODE_INVALID_REQUEST","ERROR_CODE_NETWORK_ERROR","ERROR_CODE_NO_FILL"],
-    Params:function(){
-      isTesting=false;
-      isForChild=false;
-      gender=0;//0 unknown,1 male,2 female
-      extra={};//key value pair like {'keywork':'admob phonegap','corlor':0xffffff}
-    },
-    loadInterstitial: function (param,successCallback, failureCallback) {
+    
+	MagnetInitialize: function (successCallback, failureCallback) {
         cordova.exec(
             successCallback,
             failureCallback,
-            'magnet',
-            'loadInterstitial',
+            'MagnetCordovaPlugin',
+            'initialize',
+            []
+        );
+    },
+    MagnetSetTestMode: function (testMode, successCallback, failureCallback) {
+        cordova.exec(
+            successCallback,
+            failureCallback,
+            'MagnetCordovaPlugin',
+            'setTestMode',
             [
-                param
+                {'TEST_MODE': testMode}
+            ]
+        );
+    },
+	MagnetLoadInterstitial: function (addUnitId, successCallback, failureCallback) {
+        cordova.exec(
+            successCallback,
+            failureCallback,
+            'MagnetCordovaPlugin',
+            'loadInterstitial',
+			[
+				{'AD_UNIT_ID': addUnitId}
+            ]
+        );
+    },
+    MagnetShowInterstitial: function (successCallback, failureCallback) {
+        cordova.exec(
+            successCallback,
+            failureCallback,
+            'MagnetCordovaPlugin',
+            'showInterstitial',
+            []
+        );
+    },
+    MagnetShowBanner: function (addUnitId, position, successCallback, failureCallback) {
+        cordova.exec(
+            successCallback,
+            failureCallback,
+            'MagnetCordovaPlugin',
+            'ShowBanner',
+            [
+                {'AD_UNIT_ID': addUnitId, 'BANNER_POSITION': position}
             ]
         );
     }
